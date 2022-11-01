f85e9169886019b71f519d0d5060636c92624888
GouruRK <gouru.rk.gouru.rk@gmail.com>
2022-11-01 12:11:45 +0100

    main

---
+++ minesweeper.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* * * * * * * */
+/*  Structures */
+/* * * * * * * */
+
+typedef struct _game {
+    int width;
+    int height;
+    int mines;
+    int **terrain;
+    int termine;
+} Game;
+
+/* * * * * * * */
+/*  Prototypes */
+/* * * * * * * */
+
+/* * * * * * * */
+/*  Fonctions */
+/* * * * * * * */
+
+/* * * * * * * */
+/*    main     */
+/* * * * * * * */
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
