8514f71a54b2f8930b3682de385c929e091f5a31
qlabo <q.laborde1220@gmail.com>
2022-11-11 20:08:11 +0100

    lecture des parametre + bug pas vraiment aléatoire

--- minesweeper.c
+++ minesweeper.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* * * * * * * */
 /*  Structures */
@@ -17,7 +18,9 @@ typedef struct _game {
 /*  Prototypes */
 /* * * * * * * */
 
-void lecture_arguments(int argc, char* argv[], Game* g);
+void init_jeu(int argc, char* argv[], Game* g);
+
+void init_jeu_avec_param(int argc, char* argv[], Game* g);
 
 void lecture_fichier(FILE* fichier, Game* g);
 
@@ -25,20 +28,25 @@ int lecture_param(FILE* fichier);
 
 void init_tableau(Game* g);  // avec que des 0
 
-void lecture_tableau(FILE* fichier, Game* g);  // supposons fichier bien formé.
+void ecrire_tableau(FILE* fichier, Game* g);  // supposons fichier bien formé.
+
+void ecrire_tableau_random(Game* g, int seed);
+
+void lecture_arg_j(int i, char* argv[], Game* g);
 
 void lecture(Game* g);
 
 void free_2d_tab(int** tab, int lignes);
 
+int random_n(int min, int max, int seed);
+
 /* * * * * * * */
 /*  Fonctions */
 /* * * * * * * */
 
-void lecture_arguments(int argc, char* argv[], Game* g) {
+void init_jeu(int argc, char* argv[], Game* g) {
     int fichierOK = 0;
     if (argv[argc - 1][0] != '-') {  // pas un parametre
-        printf("%s\n", argv[argc - 1]);
         FILE* f;
         f = fopen(argv[argc - 1], "r");
         if (f != 0) {  // le fichier existe
@@ -56,36 +64,63 @@ void lecture_arguments(int argc, char* argv[], Game* g) {
         }
     }
     if (fichierOK == 0) {
-        for (int i = 1; i < argc - 1; i++) {
-            if (argv[i][0] != '-') {  // pas un parametre
-                FILE* f;
-                f = fopen(argv[i], "r");
-                if (f != 0) {  // le fichier existe
-                    fprintf(stderr, "\x1B[31m" "/!\\ " "\x1B[0m" " 2 fichiers renseignés " "\x1B[31m" "/!\\" "\x1B[0m" "\n");
-                    exit(1);
-                }
-            } else {
-                printf("%c\n", argv[i][1]);
-                printf("%s\n", argv[i + 1]);
-                printf("%s\n", argv[i + 2]);
-                printf("%s\n", argv[i + 3]);
+        init_jeu_avec_param(argc, argv, g);
+    }
+}
 
-                if (argv[i][1] == 'j') {
-                    printf("%d", i);
-                    int h = atoi(argv[i + 1]), l = atoi(argv[i + 2]), m = atoi(argv[i + 3]);
-                    if (m >= l * h) {
-                        fprintf(stderr, "Trop de mines\n");
-                        exit(1);
-                    }
-                    g->height = atoi(argv[i + 1]);
-                    g->width = atoi(argv[i + 2]);
-                    g->mines = atoi(argv[i + 3]);
-                    i += 3;
-                    printf("%d", i);
-                }
+void init_jeu_avec_param(int argc, char* argv[], Game* g) {
+    int seed = time(NULL);
+    g->height = 10, g->width = 10, g->mines = 10;
+    for (int i = 1; i < argc - 1; i++) {
+        if (argv[i][0] != '-') {  // pas un parametre
+            FILE* f;
+            f = fopen(argv[i], "r");
+            if (f != 0) {  // le fichier existe
+                fprintf(stderr, "\x1B[31m /!\\ \x1B[0m 2 fichiers renseignés \x1B[31m /!\\ \x1B[0m \n");
+                exit(1);
+            }
+        } else {
+            if (argv[i][1] == 'j') {
+                lecture_arg_j(i, argv, g);
+                i += 3;
+            }
+            if (argv[i][1] == 'a') {
+                seed = atoi(argv[i + 1]);
+                i++;
             }
         }
     }
+    init_tableau(g);
+    ecrire_tableau_random(g, seed);
+}
+
+void lecture_arg_j(int i, char* argv[], Game* g) {
+    int h = atoi(argv[i + 1]), l = atoi(argv[i + 2]), m = atoi(argv[i + 3]);
+    if (m >= l * h) {
+        fprintf(stderr, "\x1B[31m/!\\ \x1B[0m Plus de mine que de case \x1B[31m /!\\ \x1B[0m \n");
+        exit(2);
+    }
+    g->height = atoi(argv[i + 1]);
+    g->width = atoi(argv[i + 2]);
+    g->mines = atoi(argv[i + 3]);
+}
+
+void ecrire_tableau_random(Game* g, int seed) {
+    int acc = 0, i = 0, j = 0;
+    while (acc != g->mines) {
+        i = random_n(0, (g->height) - 1, seed);
+        j = random_n(0, (g->width) - 1, seed);
+        printf("i=%d j=%d\n", i, j);
+        if (g->terrain[i][j] == 0) {
+            g->terrain[i][j] = 9;
+            acc++;
+        }
+    }
+}
+
+int random_n(int min, int max, int seed) {
+    srand(time(NULL));
+    return (rand() % (max - min + 1)) + min;
 }
 
 void lecture_fichier(FILE* fichier, Game* g) {
@@ -93,7 +128,7 @@ void lecture_fichier(FILE* fichier, Game* g) {
     g->width = lecture_param(fichier);
     g->mines = lecture_param(fichier);
     init_tableau(g);
-    lecture_tableau(fichier, g);
+    ecrire_tableau(fichier, g);
 }
 
 int lecture_param(FILE* fichier) {
@@ -127,7 +162,7 @@ void init_tableau(Game* g) {
     g->terrain = tab;
 }
 
-void lecture_tableau(FILE* fichier, Game* g) {
+void ecrire_tableau(FILE* fichier, Game* g) {
     for (int i = 0; i < g->height; i++) {
         for (int j = 0; j < g->width; j++) {
             fscanf(fichier, "%d", &(g->terrain[i][j]));
@@ -157,10 +192,10 @@ void free_2d_tab(int** tab, int lignes) {
 
 int main(int argc, char* argv[]) {
     Game g;
-    lecture_arguments(argc, argv, &g);
+    init_jeu(argc, argv, &g);
     lecture(&g);
     free_2d_tab(g.terrain, g.height);
-    // printf("\n");
-    // printf("h : %d, l : %d nm : %d\n", g.height, g.width, g.mines);
+    printf("\n");
+    printf("h : %d, l : %d nm : %d\n", g.height, g.width, g.mines);
     return 0;
 }
